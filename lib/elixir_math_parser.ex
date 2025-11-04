24570e7bf215c79a55927f23a6735acd8cd0ff59
Anhgelus Morhtuuzh <william@herges.fr>
2025-11-04 12:16:57 +0100

    Initial commit

---
+++ lib/elixir_math_parser.ex
@@ -0,0 +1,18 @@
+defmodule ElixirMathParser do
+  @moduledoc """
+  Documentation for `ElixirMathParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirMathParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_math_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirMathParserTest do
+  use ExUnit.Case
+  doctest ElixirMathParser
+
+  test "greets the world" do
+    assert ElixirMathParser.hello() == :world
+  end
+end
